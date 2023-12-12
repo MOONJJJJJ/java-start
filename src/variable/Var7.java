28d6a1784b1635833ea6e7ad1a94bfbfd2a87ad8
MOONJJJJJ <yangmoonjj5@gmail.com>
2023-12-12 23:39:28 +0900

    2 - 변수 타입2

--- src/variable/Var7.java
+++ src/variable/Var7.java
@@ -2,6 +2,7 @@ package variable;
 
 public class Var7 {
     public static void main(String[] args) {
+        //리터럴 : 개발자가 직접 변수에 입력해서 넣는 값
         int a  =100;
         double b = 10.5;
         boolean c = true;
