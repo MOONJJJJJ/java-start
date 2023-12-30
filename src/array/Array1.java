9d79b9af688ce805c55c0692a06dded901ac281e
MOONJJJJJ <yangmoonjj5@gmail.com>
2023-12-30 15:07:39 +0900

    8 - 배열 시작

---
+++ src/array/Array1.java
@@ -0,0 +1,12 @@
+package array;
+
+public class Array1 {
+
+    public static void main(String[] args) {
+        int a1 = 1;
+        int a2 = 2;
+        System.out.println(a1);
+        System.out.println(a2);
+
+    }
+}
